e51800042b6a5141ac7bea6671e0597da24420a0
Yush singla <yushsingla@gmail.com>
2021-10-21 20:23:48 +0530

    liked to mongodb atlas

--- helpers/init_mongodb.js
+++ helpers/init_mongodb.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
+  .connect("mongodb+srv://yush:" + process.env.MONGO_DB_PASSWORD + "@cluster0.dgbfh.mongodb.net/microsoft_teams_clone?retryWrites=true&w=majority", {
     dbName: process.env.DB_NAME,
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -9,23 +9,23 @@ mongoose
     useCreateIndex: true,
   })
   .then(() => {
-    console.log('mongodb connected.')
+    console.log("mongodb connected.");
   })
-  .catch((err) => console.log(err.message))
+  .catch((err) => console.log(err.message));
 
-mongoose.connection.on('connected', () => {
-  console.log('Mongoose connected to db')
-})
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose connected to db");
+});
 
-mongoose.connection.on('error', (err) => {
-  console.log(err.message)
-})
+mongoose.connection.on("error", (err) => {
+  console.log(err.message);
+});
 
-mongoose.connection.on('disconnected', () => {
-  console.log('Mongoose connection is disconnected.')
-})
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose connection is disconnected.");
+});
 
-process.on('SIGINT', async () => {
-  await mongoose.connection.close()
-  process.exit(0)
-})
+process.on("SIGINT", async () => {
+  await mongoose.connection.close();
+  process.exit(0);
+});
